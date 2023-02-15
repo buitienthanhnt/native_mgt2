aba0a27e86e33bdefb4bf02572cc92a11411dfd4
thanhnt <buitienthanhnt@gmail.com>
2023-02-16 01:19:38 +0700

    hotfix/React-native-mgt2: Up code for tooltip.

--- navigation/Cart.js
+++ navigation/Cart.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from "react";
-import { View, Text, Button, StatusBar, ScrollView, StyleSheet, Image, Dimensions, LogBox } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import React, { useEffect, useState } from "react";
+import { View, Text, Button, StatusBar, ScrollView, StyleSheet, Image, Dimensions, LogBox, TextInput, TouchableOpacity, Platform, FlatList } from "react-native";
+import { Tooltip } from 'react-native-elements'; //npm install react-native-elements + npm i --save react-native-vector-icons + npm install react-native-safe-area-context
+// npm install react-native-paper
 import { connect } from "react-redux";
 import cart_val from "./cart_data";
 
@@ -16,19 +17,28 @@ const Cart = (props) => {
         return (
             <View style={{ flex: 1 }}>
                 <StatusBar></StatusBar>
-                <Text style={css.cart_title}>cart data</Text>
-                <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    showsHorizontalScrollIndicator={false}
-                >
-                    <View >
-                        <Cart_item items={cart_data.items} ></Cart_item>
-                    </View>
-                </ScrollView>
-                <Cart_price prices={cart_data.prices}></Cart_price>
-                <Button title="show cart_data" onPress={() => {
+                <View style={{ flex: 70 }}>
+                    <Text style={css.cart_title}>cart data</Text>
+                    <ScrollView
+                        showsVerticalScrollIndicator={false}
+                        showsHorizontalScrollIndicator={false}
+                    >
+                        <View>
+                            <Cart_item items={cart_data.items} ></Cart_item>
+                        </View>
+                    </ScrollView>
+                </View>
+                <View style={{ flex: 30 }}>
+                    <Cart_price prices={cart_data.prices}></Cart_price>
+                </View>
+
+                {/* <Button title="show cart_data" onPress={() => {
                     console.log(cart_data);
-                }}></Button>
+                }}></Button> */}
+                {/* skipAndroidStatusBar={true} để không bị lặp lại nội dung khi hiển thị. */}
+                 <Tooltip popover={<Text>tooooool</Text>} withOverlay={false} withPointer={true} skipAndroidStatusBar={true}>
+                    <Text style={{ fontSize: 17, backgroundColor: "red", alignSelf: 'flex-start' }}>asdadsd</Text>
+                    </Tooltip>
             </View>
         )
     }
@@ -52,7 +62,9 @@ const Cart = (props) => {
 };
 
 const Cart_item = (props) => {
-    console.log(props);
+    // useEffect(()=>{
+    //     setQty(item.item_qty)
+    // }, []);
     return (
         <View style={css.cart_item}>
             <FlatList
@@ -60,26 +72,74 @@ const Cart_item = (props) => {
                 keyExtractor={(item, index) => { return String(index); }}
                 renderItem={({ item }) => {
                     return (
-                        <View>
-                            <View style={{ padding: 8, flexDirection: "row" }}>
-                                <View style={{ width: "45%" }}>
-                                    <Image source={{ uri: item.image_path }} style={css.item_image} resizeMode="cover"></Image>
-                                </View>
-                                <View style={{ width: "50%" }}>
-                                    <Text style={{ fontSize: 17 }}>{item.name}</Text>
-                                    <Text style={{ fontSize: 17 }}>qty: {item.item_qty}</Text>
-                                </View>
-                            </View>
-                            <View style={{ flexDirection: "row", justifyContent: "center" }}>
-                                <View style={{ height: 1, backgroundColor: "black", width: "90%" }}></View>
-                            </View>
-                        </View>
+                        <Item_data {...item}></Item_data>
                     )
                 }}
             ></FlatList>
         </View>
     );
 };
+
+const Item_data = (props) => {
+    const [qty, setQty] = useState(0);
+    useEffect(() => {
+        setQty(props.item_qty);
+    }, []);
+    return (
+        <View>
+            <View style={{ padding: 8, flexDirection: "row" }}>
+                <View style={{ width: "45%" }}>
+                    <Image source={{ uri: props.image_path }} style={css.item_image} resizeMode="cover"></Image>
+                </View>
+                <View style={{ width: "50%" }}>
+                    <Tooltip popover={<Text>{props.name}</Text>} withOverlay={false} skipAndroidStatusBar={true}>
+                        <Text style={{ fontSize: 17 }}>{props.name}</Text>
+                    </Tooltip>
+                    <Text style={{ fontSize: 17 }}>price: {props.prices[0].value}</Text>
+                    <View style={{ flexDirection: "row" }}>
+                        {/* <Text style={{ fontSize: 17, fontWeight: "bold" }}>qty: {item.item_qty}</Text> */}
+                        <View style={{ textAlign: "center", justifyContent: "center" }}>
+                            <TouchableOpacity onPress={() => {
+                                console.log("sub qty");
+                                if (qty >= 1) {
+                                    let _qty = qty;
+                                    setQty(_qty -= 1)
+                                }
+                            }}>
+                                <Text style={{ fontSize: 20, fontWeight: "900", marginRight: 6 }}>-</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <TextInput
+                            value={String(qty)}
+                            // defaultValue={String(item.item_qty)} // không chạy
+                            onChangeText={(value) => { setQty(value) }}
+                            style={{ width: 30, height: 30, borderWidth: 1, borderRadius: 6, padding: 4, paddingLeft: 10 }}
+                            keyboardType="numeric">
+                        </TextInput>
+                        <View style={{ textAlign: "center", justifyContent: "center" }}>
+                            <TouchableOpacity onPress={() => {
+                                console.log("add qty");
+                                let _qty = qty;
+                                setQty(_qty += 1);
+                            }}>
+                                <Text style={{ fontSize: 20, paddingLeft: 5, fontWeight: "600" }}>+</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <View style={{ textAlign: "center", justifyContent: "center", width: Platform.OS == "web" ? "-webkit-fill-available" : "60%" }}>
+                            <View style={{ alignItems: Platform.OS != "web" ? "flex-end" : "none" }}>
+                                <Text style={{ fontSize: 20 }}>{qty * props.prices[0].value}</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                <View style={{ height: 1, backgroundColor: "black", width: "100%" }}></View>
+            </View>
+        </View>
+    );
+}
 
 const Cart_price = (props) => {
     return (
@@ -105,19 +165,20 @@ const Cart_price = (props) => {
 
 const css = StyleSheet.create({
     cart_title: {
-        fontSize: 20,
+        fontSize: 24,
         textAlign: "center",
-        flexDirection: "row",
+        // flexDirection: "row",
         color: "tomato"
     },
     cart_item: {
         width: "100%",
-        marginTop: 8,
+        // marginTop: 4,
         // backgroundColor: "violet"
     },
     item_image: {
         width: Dimensions.get("window").width / 100 * 40,
-        height: Dimensions.get("window").width / 100 * 40
+        height: Dimensions.get("window").width / 100 * 40,
+        borderRadius: 20
     }
 });
 
